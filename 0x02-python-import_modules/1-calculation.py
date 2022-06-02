7d88941ed6db43658f2553039190806e1917c23d
binael <nbinael@yahoo.com>
2022-06-02 13:39:49 -0700

    message

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name == '__main__':
+    import calculator_1 as calc
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calc.div(a, b)))
