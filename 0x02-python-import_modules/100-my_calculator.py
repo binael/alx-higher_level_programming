37c7a2b678c19f6b42783d5e8e9cc02fa3fe79f4
binael <nbinael@yahoo.com>
2022-06-02 16:17:17 -0700

    message

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+
+def calculate(argv):
+    """A basic calculator"""
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if argv[2] not in "-+/*":
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if argv[2] == "+":
+        print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, add(a, b)))
+    elif argv[2] == "-":
+        print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, sub(a, b)))
+    elif argv[2] == "*":
+        print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, mul(a, b)))
+    else:
+        print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, div(a, b)))
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    calculate(sys.argv)
