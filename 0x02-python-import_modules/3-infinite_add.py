6234af55416eb5b9d04153e58d41106d885e090b
binael <nbinael@yahoo.com>
2022-06-02 14:46:40 -0700

    message

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def inf_add(argv):
+    length = len(argv) - 1
+    i = 1
+    total = 0
+
+    while i <= length:
+        total += int(argv[i])
+        i += 1
+
+    print(total)
+
+if __name__ == "__main__":
+    
+    import sys
+    inf_add(sys.argv)
