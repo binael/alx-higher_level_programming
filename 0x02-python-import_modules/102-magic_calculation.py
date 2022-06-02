dc40ad2d292cc45c2538d3e035ce1477461d0033
binael <nbinael@yahoo.com>
2022-06-02 16:42:15 -0700

    message

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -9,7 +9,7 @@ def def magic_calculation(a, b):
         c = add(a, b)
 
         for i in range(4, 6):
-            c = add(i, c)
+            c = add(c, i)
         return (c)
     else:
         return sub(a, b)
