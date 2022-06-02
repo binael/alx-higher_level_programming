eb84cdf7c9b9fa4bfb8a5dd773a24abcc29ac779
binael <nbinael@yahoo.com>
2022-06-02 11:42:10 -0700

    message

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-from add_0 import add
+if __name__ == '__main__':
 
-a = 1
-b = 2
+    from add_0 import add
 
-if __name__ = "__main__":
+    a = 1
+    b = 2
+
     print(f"{a:d} + {b:d} = {add(a, b):d"}
