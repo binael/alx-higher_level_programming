903fe90350f92be7b549eca32d677c500de67d6a
binael <nbinael@yahoo.com>
2022-06-02 14:09:45 -0700

    message

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from sys import argv
+
+    length = len(argv)
+
+    if length > 2:
+        print("{} arguments:".format(length - 1))
+    elif length == 2:
+        print("{} argument:".format(length - 1))
+    else:
+        print("{} argument.".format(length - 1))
+
+    for i in range(1, length):
+        print("{}: {}".format(i, sys.argv[i]))
