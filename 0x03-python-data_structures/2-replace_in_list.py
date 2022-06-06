a00d7aa7dcc40de6d31c1e2a3959e139301a0bec
binael <nbinael@yahoo.com>
2022-06-06 00:19:35 -0700

    message

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    """Modify a list element"""
+
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    my_list[idx] = element
+
+    return my_list
