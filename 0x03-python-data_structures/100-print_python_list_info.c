f2c3fbae98ba9f4e5c0006a204a190643e6ef6bb
binael <nbinael@yahoo.com>
2022-06-06 16:49:33 -0700

    message

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -13,20 +13,16 @@
 void print_python_list_info(PyObject *p)
 {
 	Py_ssize i, size;
-	PyObject *py_list;
-	const char *list_type;
 	PyListObject *object;
 
 	object = (PyListObject *)p;
 	size = PyList_Size(p);
 
-	printf("[*] Size of the Python List = %d\n", (int)size);
+	printf("[*] Size of the Python List = %d\n", size);
 	printf("[*] Allocated = %d\n", (int)object->allocated);
 
 	for (i = 0; i < size; i++)
 	{
-		py_list = PyList_GetItem(p, i);
-		list_type = Py_TYPE(py_list)->tp_name;
-		printf("Element %d: %s\n", (int)i, list_type);
+		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i)->tp_name);
 	}
 }
