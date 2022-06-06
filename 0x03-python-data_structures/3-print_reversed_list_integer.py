8c9835267da8bf64f87ba4f2cd15278350ee4e71
binael <nbinael@yahoo.com>
2022-06-06 00:41:29 -0700

    message

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,7 @@
 def print_reversed_list_integer(my_list=[]):
     """Reverse printing a  list of integers"""
 
-    if my_list.isempty():
+    if isempty(my_list):
         return
 
     for i in reversed(my_list):
