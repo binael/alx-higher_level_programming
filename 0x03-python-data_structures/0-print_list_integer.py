af6a3a385589138168d139d8cb36b6f5663af45a
binael <nbinael@yahoo.com>
2022-06-06 00:08:12 -0700

    message

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,8 +3,5 @@
 def print_list_integer(my_list=[]):
     """ Prints a list of integers"""
 
-    if my_list.isempty():
-        return
-
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
