99a23eee17b9fe86dd422735094666f43505a8db
binael <nbinael@yahoo.com>
2022-06-06 01:37:42 -0700

    message

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,12 @@
 def print_matrix_integer(matrix=[[]]):
     """Prints a matrix element"""
 
+    if not matrix:
+        return
+
     for i in range(len(matrix)):
         for j in range(len(matrix[1])):
-            print("{:d}".format(matrix[i][j], end=""))
+            print("{:d}".format(matrix[i][j]), end="")
 
             if j < len(matrix[1]) - 1:
                 print(" ", end="")
