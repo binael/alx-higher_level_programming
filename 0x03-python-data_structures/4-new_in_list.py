7fbfe576051a75f52814dbd8606e10bfe3e32656
binael <nbinael@yahoo.com>
2022-06-06 01:06:39 -0700

    message

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,12 +6,7 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    new_list = []
-
-    for i in range(len(my_list)):
-        if i == idx:
-            new_list.append(element)
-            continue
-        new_list.append(i)
+    new_list = my_list[:]
+    new_list[idx] = element
 
     return new_list
