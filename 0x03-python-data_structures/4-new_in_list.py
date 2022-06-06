8c9a1b4cc8dd912b053529b850fb7a59605c902e
binael <nbinael@yahoo.com>
2022-06-06 00:58:09 -0700

    message

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """ Modifies a  list and storing in a new list"""
+
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    new_list = []
+
+    for i in range(len(my_list)):
+        if i == idx:
+            new_list.append(element)
+            continue
+        new_list.append(i)
