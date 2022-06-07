132aa8a6b89a26639cd97d16e4f8fe0491cce3c4
binael <nbinael@yahoo.com>
2022-06-07 00:24:04 +0000

    message

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -14,10 +14,8 @@ void print_python_list_info(PyObject *p)
 {
 	int i;
 
-	PyListObject *object = (PyListObject *)p;
-
 	printf("[*] Size of the Python List = %d\n", Py_SIZE(p));
-	printf("[*] Allocated = %d\n", object->allocated);
+	printf("[*] Allocated = %d\n", ((PyListObject *)p)->allocated);
 
 	for (i = 0; i < Py_SIZE(p); i++)
 	{
