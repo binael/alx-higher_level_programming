79ad6974e4d2eb5f03e772e1f74ba9958d72a420
binael <nbinael@yahoo.com>
2022-06-08 08:02:15 -0700

    message

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    """Multiply all values by 2"""
+    
+    new_dict = {}
+
+    for key in a_dictionary.keys():
+        new_dict.setdefault(key, a_dictionary[key] * 2)
+
+    return new_dict
