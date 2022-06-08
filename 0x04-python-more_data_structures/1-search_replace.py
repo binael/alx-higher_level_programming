87e9ad043da5b09b3eb49a09955da3919d832431
binael <nbinael@yahoo.com>
2022-06-08 07:06:24 -0700

    message

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,6 +5,8 @@ def search_replace(my_list, search, replace):
 
     new_list = my_list[:]
 
-    new_list[search] = replace
+    for i in range(len(my_list)):
+        if new_list[i] == search:
+            new_list[search] = replace
 
     return new_list
