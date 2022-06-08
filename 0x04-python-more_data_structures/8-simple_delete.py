9e7beeab7be58395378fa4cef0e463775ee54e65
binael <nbinael@yahoo.com>
2022-06-08 07:51:28 -0700

    message

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    """Deletes a key from a dictionary"""
+
+    if key in a_dictionary.keys():
+        del a_dictionary[key]
+
+    return a_dictionary
