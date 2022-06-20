629080e6180bc81a77fef9cd4f15236af2a88a57
binael <nbinael@yahoo.com>
2022-06-20 11:12:33 -0700

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,9 @@
 def safe_print_division(a, b):
     try:
         div = a / b
-    except:
+        print("inside result: {:.1f}".format(div))
+    except Exception:
         div = None
-    finally:
         print("inside result: {}".format(div))
+    finally:
         return(div)
