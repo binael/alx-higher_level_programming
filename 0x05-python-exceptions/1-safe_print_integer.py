2efe11aae64a4d660c283a3be91b33ea334472c5
binael <nbinael@yahoo.com>
2022-06-20 10:16:57 -0700

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        a = int(value)
+        print("{:d}".format(a))
+        return True
     except:
         return False
-    print("{:d}".format(a))
-    return True
