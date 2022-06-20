1c6772abf1a326e0186bb9db70b2d4f1f10296ad
binael <nbinael@yahoo.com>
2022-06-20 14:20:27 -0700

    task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except Exception as err:
-        print("Exception: {}".format(err))
+        print("Exception: {}".format(err), file=sys.stderr)
         return False
