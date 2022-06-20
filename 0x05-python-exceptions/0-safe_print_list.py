6200df7b23f7a7f9f0cbee1a7d128ccb37706f49
binael <nbinael@yahoo.com>
2022-06-20 10:00:43 -0700

    safe print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        counter = 0
+        for i in my_list:
+            counter += 1
+        a = my_list[x]
+    except IndexError:
+        x = counter
+    
+    for i in range(x):
+        print(my_list[i], end="")
+    print("")
+    return x
