dee20e4d1e7d5bbc05e3cf509be79a1a02a5025a
binael <nbinael@yahoo.com>
2022-06-21 14:20:00 -0700

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Creating a class Square"""
+
+
+class Square:
+    """A class that takes a square object"""
+
+    def __init__(self, size=0):
+        """initialize the instances of the class"""
+        if not type(size) == int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square"""
+        return (self.__size ** 2)
