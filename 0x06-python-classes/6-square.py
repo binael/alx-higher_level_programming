6f1371227994e76937e32f8c592c374192535480
binael <nbinael@yahoo.com>
2022-06-21 15:24:15 -0700

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,6 +46,8 @@ class Square:
         """Prints the square using #"""
         if self.__size == 0:
             print("")
+            return
+
         [print("") for i in range(self.__position[1])]
         for i in range(self.__size):
             print(f"{' ' * self.__position[0]}{'#' * self.__size}")
