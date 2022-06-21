25db15c37fbf9661407e760f05b058b4de2c58f4
binael <nbinael@yahoo.com>
2022-06-21 13:49:52 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Creating a class Square"""
+
+
+class Square:
+    """A class that takes a square object"""
+
+    def __init__(self, size):
+        """initialize the instances of the class"""
+        self.__size = size
