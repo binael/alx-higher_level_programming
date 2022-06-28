39e33c954d89747458e4b72dead80dfb8f9e9089
Binael <nbinael@yahoo.com>
2022-06-28 01:18:41 +0100

    added del method

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""An Rectangle class"""
+
+
+class Rectangle:
+    """A class that models a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes both height and width to zero"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Gets the width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Get the height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Sets the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    """
+    Calculates the area of the rectangle
+    """
+    def area(self):
+        return (self.__width * self.__height)
+
+    """
+    Calculates the perimeter of the rectangle
+    """
+    def perimeter(self):
+        if not self.area():
+            return 0
+        else:
+            return (2 * (self.__width + self.__height))
+
+    def __str__(self):
+        """Returns a printable object"""
+        if not self.area():
+            return ""
+        for i in range(self.__height - 1):
+            print(f"{'#' * self.__width}")
+        print(f"{'#' * self.__width}", end="")
+        return ""
+
+    def __repr__(self):
+        """Returns the string representation of the rectangle"""
+        string = "Rectangle(" + str(self.__width) + ", "
+        string += str(self.__height) + ")"
+        return (string)
+
+    def __del__(self):
+        """Deletes an instance of the rectangle"""
+        print("Bye rectangle...")
