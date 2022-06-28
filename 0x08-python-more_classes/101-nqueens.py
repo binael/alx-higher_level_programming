6643e61111b4dc22cf5afd6afe74a553a41873ee
Binael <nbinael@yahoo.com>
2022-06-28 02:55:25 +0100

    outline of nqueen

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""placing N non-attacking queens on an N×N chessboard"""
+
+def nqueen(N):
+    """Function that solves the nqueen problem"""
+    pass
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) is not 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        number = int(sys.argv[1])
+    except Exception:
+        print("N must be a number")
+        sys.exit(1)
+
+    if number is not float(sys.argv[1]):
+        print("N must be a number")
+        sys.exit(1)
+
+    if number < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    nqueen(number)
