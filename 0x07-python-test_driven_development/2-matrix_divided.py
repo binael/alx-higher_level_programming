c2585cdc1822b78195874f9d0583a043fa56016a
Binael <nbinael@yahoo.com>
2022-07-12 11:18:24 +0100

    task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,7 +3,11 @@
 
 
 def matrix_divided(matrix, div):
-    """Divides every element of a matrix by int/float div"""
+    """Divides every element of a matrix by int/float div
+    args:
+        matrix: A matrix where all elements are either float or int
+        div (int or float): divides all the matrix elements
+        """
 
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
@@ -13,7 +17,7 @@ def matrix_divided(matrix, div):
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not all(type(num) in [int, float] for row in matrix for num in row):
-        text "matrix must be a matrix (list of lists) of integers/floats"
+        text = "matrix must be a matrix (list of lists) of integers/floats"
         raise TypeError(text)
 
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
