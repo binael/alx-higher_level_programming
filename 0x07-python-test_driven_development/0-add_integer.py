db8dd5b0c5aaeb85220043e3db0f58812061bd6c
Binael <nbinael@yahoo.com>
2022-07-12 10:25:15 +0100

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,7 +3,11 @@
 
 
 def add_integer(a, b=98):
-    """Adds two numbers"""
+    """Adds two numbers
+    args:
+    a - integer or float
+    b - optional integer whose base value is 98
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
