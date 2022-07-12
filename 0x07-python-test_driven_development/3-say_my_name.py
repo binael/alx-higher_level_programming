43587977a6a63168feed2f1e274f3bdfd246a85e
Binael <nbinael@yahoo.com>
2022-07-12 12:10:02 +0100

    task 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,7 +5,7 @@
 def say_my_name(first_name, last_name=""):
     """Accepts only string of last name and first name"""
     if type(first_name) is not str:
-        raise TypeError("first name must be a string")
+        raise TypeError("first_name must be a string")
     if type(last_name) is not str:
-        raise TypeError("last name must be a string")
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
