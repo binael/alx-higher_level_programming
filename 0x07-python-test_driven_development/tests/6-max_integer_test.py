2cba604407bc9bb1e63a5ead2a24004fc6f15869
Binael <nbinael@yahoo.com>
2022-07-12 13:39:15 +0100

    task 5

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -14,13 +14,12 @@ class TestMaxInteger(unittest.TestCase):
         self.assertRaises(TypeError, max_integer, 2)
         self.assertRaises(TypeError, max_integer, -2)
         self.assertRaises(TypeError, max_integer, 1 + 2j)
-        self.assertRaises(TypeError, max_integer, ["2", 1j, 5])
+        self.assertRaises(TypeError, max_integer, ["2", 3j, 5])
         self.assertRaises(TypeError, max_integer, 2.2)
-        self.assertRaises(TypeError, max_integer, )
 
     def test_normalResults(self):
         self.assertEqual(max_integer([1, 2, 3, 4, 5, 2, 8, 10, 5, 9]), 10)
-        self.assertEqual(max_integer([1, 7, -38, 5, 33, -40, 8]), 33)
+        self.assertEqual(max_integer([1, 7, 32, 5, 33, -40, 8]), 33)
         self.assertEqual(max_integer([-20, -41, -3, -67, -18]), -3)
         self.assertEqual(max_integer([55, 8, 7, 19, 20]), 55)
         self.assertEqual(max_integer([-20, -41, -333, -67, -18]), -18)
