4a749ae5a8a4b567bc373631fb698b5742e767e8
Binael <nbinael@yahoo.com>
2022-07-12 13:07:02 +0100

    task 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,7 +3,7 @@
 
 
 def text_indentation(text):
-    """Prints a text with two new lines after 
+    """Prints a text with two new lines after
     special characters
     Args:
         text (string): The text to print.
@@ -12,8 +12,15 @@ def text_indentation(text):
 
     """
     if type(text) is not str:
-        raise TypeError("text must be string")
+        raise TypeError("text must be a string")
+
+    flag = False
     for i in text:
+        if not flag:
+            if not i.strip():
+                continue
+            flag = True
         print(i, end='')
         if i in ".?:":
             print('\n\n', end='')
+            flag = False
