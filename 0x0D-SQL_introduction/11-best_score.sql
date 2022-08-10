c7ffe625074542e10f9309812395e54e0f524a7d
Binael <nbinael@yahoo.com>
2022-08-10 01:46:51 +0100

    task 11 to 16

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- A script that lists all records with a score >= 10 in the table
+-- second_table of the database hbtn_0c_0 in your MySQL server.
+SELECT score, name
+	FROM hbtn_0c_0
+		WHERE score >= 10
+			ORDER BY score DESC;
