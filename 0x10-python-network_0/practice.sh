821ec8cbdb8c8f55bda40e789c154e679f8bd4d2
binael <nbinael@yahoo.com>
2023-04-27 17:10:41 +0100

    added new practice file to check for corrections

--- 0x10-python-network_0/practice.sh
+++ 0x10-python-network_0/practice.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#  takes in a URL, sends a GET request to the URL, and displays the body of the response
+
+# script
 
 status=$(curl -s -I "$1" | head -n 1 | cut -d " " -f 2)
 
@@ -7,4 +8,4 @@ if [ "$status" != 200 ]; then
 	exit
 fi
 
-curl -sL "$1"
+curl -s -L "$1" | wc -c
