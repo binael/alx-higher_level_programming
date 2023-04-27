6f8067a4704dad06a60e3c7e9cd2e80c3ee70ad7
binael <nbinael@yahoo.com>
2023-04-27 18:22:21 +0100

    modified answer

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 #  takes in a URL, sends a GET request to the URL, and displays the body of the response
 
-status=$(curl -s -I "$1" | head -n 1 | cut -d " " -f 2)
-
-if [ "$status" != 200 ]; then
-	exit
-fi
-
 curl -sL "$1"
