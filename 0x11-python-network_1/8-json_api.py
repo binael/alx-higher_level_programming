e5210f1e04d50f51d63518e58d1f1ca348ffd530
binael <nbinael@yahoo.com>
2023-04-28 21:24:33 +0100

    made more corrections

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     url = "http://0.0.0.0:5000/search_user"
 
-    if sys.argv[1]:
+    if len(sys.argv) > 1:
         data = {"q": sys.argv[1]}
     else:
         data = {"q": ""}
@@ -23,5 +23,5 @@ if __name__ == "__main__":
             print("No result")
         else:
             print("[{}] {}".format(j_file.get("id"), j_file.get("name")))
-    except Exception:
+    except ValueError:
         print("Not a valid JSON")
