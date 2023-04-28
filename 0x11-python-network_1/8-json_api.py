f06475e1e76f7b003b7091d793e1c3fba9b227ed
binael <nbinael@yahoo.com>
2023-04-28 21:09:32 +0100

    handling json files

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
 """A script that utilizes python 'requests' module to
-send a request to a url while printing the error code"""
+send a post request to a url(http://0.0.0.0:5000/search_user)
+with parameters and output results only received in
+json format"""
 
 if __name__ == "__main__":
     import requests
     import sys
 
-    url = sys.argv[1]
-    response = requests.get(url)
-
-    if response.status_code >= 400:
-        print("Error code: {}".format(response.status_code))
+    url = "http://0.0.0.0:5000/search_user"
+    
+    if sys.argv[1]:
+        data = {"q": sys.argv[1]}
     else:
-        print(response.text)
+        data = {"q": ""}
+
+    response = requests.post(url, data=data)
+
+    try:
+        j_file = response.json()
+        if j_file is None:
+            print("No result")
+        else:
+            print("[{}] {}".format(j_file.get("id"), j_file.get("name")))
+    except Exception:
+        print("Not a valid json file")
