724acab3e44f6cd46ad22f60b6e0bde3016efd9c
binael <nbinael@yahoo.com>
2023-04-28 20:41:56 +0100

    passing parameters

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A script that utilizes python 'requests' module to
+send a post request taking a URL and an email"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    params = {'email': sys.argv[2]}
+
+    response = requests.post(url, params=params)
+    print(response.text)
