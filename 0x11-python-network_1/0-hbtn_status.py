f0bc4bce5494ea3f177f4c457cec52e146b1bbd3
binael <nbinael@yahoo.com>
2023-04-28 16:57:33 +0100

    project-whats my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A python script that uses the module
+'urllib' module to fetch https://alx-intranet.hbtn.io/status
+while formatting the output
+"""
+
+if __name__ == "__main__":
+    import urllib.request
+    my_url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(my_url) as response:
+        response = response.read()
+        print("Body response:$")
+        print("\t- type: {}".format(type(response)))
+        print("\t- type: {}".format(response))
+        print("\t- type: {}".format(response.decode("utf-8")))
