aa28ea3e8b24e25bcf7216cf5a53d23a58ca0752
binael <nbinael@yahoo.com>
2023-06-13 23:50:00 +0100

    update

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-exports.converter = function (base) {
-  return function convert (num) {
-    return num.toString(base);
-  };
-};
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-exports.esrever = function (list) {
-  return list.reduceRight(function (arr, last) {
-    return (arr = arr.concat(last));
-  }, []);
-};
