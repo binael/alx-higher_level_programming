8eb45ceaafd70aa51508f96b512808df19d3a66c
binael <nbinael@yahoo.com>
2023-06-13 22:50:12 +0100

    recursive function: factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (num === 1 || num === 0 || isNaN(num)) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
+}
+console.log(factorial(parseInt(process.argv[2])));
