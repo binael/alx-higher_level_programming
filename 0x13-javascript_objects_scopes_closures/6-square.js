5fb215845f0b9164fcf3df6bc47db0fc5982916d
binael <nbinael@yahoo.com>
2023-06-14 20:04:49 +0100

    added print to the square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square.js') {
   charPrint (c = 'X') {
-    if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        let row = '';
-        for (let j = 0; j < this.height; j++) {
-          row += c;
-        }
-        console.log(row);
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.height; j++) {
+        row += c;
       }
+      console.log(row);
     }
   }
 };
