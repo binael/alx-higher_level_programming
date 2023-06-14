b0f1d0a5aba50d43e18ca2c0f5815858b53cdc89
binael <nbinael@yahoo.com>
2023-06-14 19:34:06 +0100

    added double and rotate function

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
--- 0x13-javascript_objects_scopes_closures/main.js
+++ 0x13-javascript_objects_scopes_closures/main.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
-const Rectangle = require('./3-rectangle');
+const Rectangle = require('./4-rectangle');
 
 const r1 = new Rectangle(2, 3);
+console.log('Normal:');
 r1.print();
 
-const r2 = new Rectangle(10, 5);
-r2.print();
+console.log('Double:');
+r1.double();
+r1.print();
+
+console.log('Rotate:');
+r1.rotate();
+r1.print();
