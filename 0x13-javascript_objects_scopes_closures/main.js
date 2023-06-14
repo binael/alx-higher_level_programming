ad3c4f8984ab5dbdaf6d5bd16427dd69fa66d64b
binael <nbinael@yahoo.com>
2023-06-14 19:27:54 +0100

    add print to the rectangle

--- 0x13-javascript_objects_scopes_closures/main.js
+++ 0x13-javascript_objects_scopes_closures/main.js
@@ -1,22 +1,8 @@
 #!/usr/bin/node
-const Rectangle = require('./2-rectangle');
+const Rectangle = require('./3-rectangle');
 
 const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
+r1.print();
 
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
-
-const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
-
-const r4 = new Rectangle(2, 0);
-console.log(r4);
-console.log(r4.width);
-console.log(r4.height);
+const r2 = new Rectangle(10, 5);
+r2.print();
