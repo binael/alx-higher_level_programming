3deb6b0bfdea6cc06f9274be3adcf620d7a4ce71
binael <nbinael@yahoo.com>
2023-06-18 22:57:09 +0100

    adjusted for case sensitivity using BINARY keyword

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host='localhost', port=3306, user=username,
                            passwd=password, db=database)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%'")
     table = cur.fetchall()
 
     for row in table:
