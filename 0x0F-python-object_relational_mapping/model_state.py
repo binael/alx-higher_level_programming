83241fd03d975eba906c25a819ec581d6c060a27
binael <nbinael@yahoo.com>
2023-06-20 03:14:33 +0100

    testing

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,17 +5,9 @@ manage the instances of mysql database relations
 """
 
 
-from sqlalchemy import create_engine
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
-import sys
 
-user = "root"
-pwd = "root"
-db = "hbtn_0e_6_usa"
-
-query = "mysql+mysqldb://{}:{}@localhost:3306/{}"
-engine = create_engine(query.format(user, pwd, db), pool_pre_ping=True)
 Base = declarative_base()
 
 
@@ -28,6 +20,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
-
-Base.metadata.create_all(engine)
