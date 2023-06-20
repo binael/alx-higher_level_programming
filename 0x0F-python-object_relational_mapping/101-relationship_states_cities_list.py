e22ad6d1bfe3e8919e2c4958c24e107abd68c3f3
binael <nbinael@yahoo.com>
2023-06-20 23:39:38 +0100

    added tabulation

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -28,4 +28,4 @@ if __name__ == "__main__":
     for state in query:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("	{}: {}".format(city.id, city.name))
+            print("\t{}: {}".format(city.id, city.name))
