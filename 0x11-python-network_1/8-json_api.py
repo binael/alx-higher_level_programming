1edb850eb5cefa587fb7f5b644b0d7453ef02593
binael <nbinael@yahoo.com>
2023-07-01 20:20:07 +0100

    posted data and got a json formattable response

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,4 +6,23 @@ with the letter as a parameter.
 """
 
 if __name__ == '__main__':
-    pass
+    import requests
+    import sys
+
+    url = 'http://0.0.0.0:5000/search_user'
+    payload = {'q': ''}
+
+    if len(sys.argv) >= 2:
+        payload = {'q': sys.argv[1]}
+
+    req = requests.post(url, data=payload)
+
+    try:
+        form = req.json()
+    except ValueError:
+        print('Not a valid JSON')
+    else:
+        if form:
+            print('[{}] {}'.format(form.get('id'), form.get('name')))
+        else:
+            print('No result')
