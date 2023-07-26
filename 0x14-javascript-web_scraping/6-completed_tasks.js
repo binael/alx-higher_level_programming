31beee923c30a70f3c8f52c81c47355c366f5181
binael <nbinael@yahoo.com>
2023-07-26 19:27:13 +0100

    formatting json file from an api

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const urlPath = process.argv[2];
+
+request.get(urlPath, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const objFile = {};
+
+    for (const user of data) {
+      const id = '' + user.userId;
+      if (!objFile[id]) {
+        objFile[id] = 0;
+      }
+      if (user.completed === true) {
+        objFile[id] += 1;
+      }
+    }
+    console.log(objFile);
+  }
+});
