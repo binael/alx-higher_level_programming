8468cd7553372a91e75f27baf24a9fe31960bb9b
binael <nbinael@yahoo.com>
2023-07-26 17:05:08 +0100

    semistandard fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const text = process.argv[3] + '\n';
 
-fs.writeFile(filePath, text, {encoding: 'utf-8'}, (err) => {
+fs.writeFile(filePath, text, { encoding: 'utf-8' }, (err) => {
   if (err) {
     console.error(err);
   }
