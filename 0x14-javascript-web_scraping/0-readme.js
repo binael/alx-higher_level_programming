b274e48f24900b9c47b3f82956d4cf277672c519
binael <nbinael@yahoo.com>
2023-07-26 16:50:03 +0100

    semistandard fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,8 +5,8 @@ const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-	console.error(err);
+    console.error(err);
   } else {
-	console.log(data);
+    console.log(data);
   }
 });
