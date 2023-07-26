0436da5843530cfbdb60dacaea1f2775490642c2
binael <nbinael@yahoo.com>
2023-07-26 16:48:20 +0100

    read from a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+	console.error(err);
+  } else {
+	console.log(data);
+  }
+});
