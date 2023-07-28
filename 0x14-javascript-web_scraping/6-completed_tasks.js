59654445221c021b91b643a0fbc407bb12152564
binael <nbinael@yahoo.com>
2023-07-28 15:34:56 +0100

    stringified the object

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -11,7 +11,7 @@ request.get(urlPath, (error, response, body) => {
     const objFile = {};
 
     for (const user of data) {
-      const id = user.userId;
+      const id = '' + user.userId;
       if (!objFile[id]) {
         objFile[id] = 0;
       }
@@ -19,7 +19,7 @@ request.get(urlPath, (error, response, body) => {
         objFile[id] += 1;
       }
     }
-
-    console.log(objFile);
+    const newObj = JSON.stringify(objFile);
+    console.log(newObj);
   }
 });
